22ef586b933721fe469c2b6b7c892da99594429e
Mohamed Ghoneim <mghoneim@microsoft.com>
2017-05-19 13:32:30 -0700

    Module_1, Contracts and Services

---
+++ GeoLib.Tests/ManagerTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using GeoLib.Contracts;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using GeoLib.Data;
+using GeoLib.Services;
+
+namespace GeoLib.Tests
+{
+    [TestClass]
+    public class ManagerTests
+    {
+        [TestMethod]
+        public void test_zip_code_retrieval()
+        {
+            Mock<IZipCodeRepository> mockZipCodeRepository = new Mock<IZipCodeRepository>();
+
+            ZipCode zipCode = new ZipCode()
+            {
+                City = "LINCOLN PARK",
+                State = new State() {Abbreviation = "NJ"},
+                Zip = "07035"
+            };
+
+            mockZipCodeRepository.Setup(obj => obj.GetByZip("07035")).Returns(zipCode);
+
+            IGeoService geoService = new GeoManager(mockZipCodeRepository.Object);
+
+            ZipCodeData data = geoService.GetZipInfo("07035");
+
+            Assert.AreEqual("LINCOLN PARK", data.City.ToUpper());
+            Assert.AreEqual("NJ", data.State);
+        }
+    }
+}
